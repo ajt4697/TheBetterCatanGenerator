89198086a9c7cf72a342cfe1a3016abe69c88eb2
Arianna Tuwiner <atuwiner97@gmail.com>
2019-06-19 18:18:53 -0400

    Trying to build basic structure, added canvas and one button

--- CatanGen/src/app/app.component.ts
+++ CatanGen/src/app/app.component.ts
@@ -7,4 +7,19 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'CatanGen';
+
+  // When Generate Board button is clicked, this function is called.
+  onClick() {
+      console.log('Button has been clicked!');
+      // When this button is clicked, ideally it will either:
+      // 1. Generate the board
+      // 2. Call the appropriate generate function
+
+      let canvas = <HTMLCanvasElement> document.getElementById('board');
+      let context = canvas.getContext('2d');
+      const img = new Image();
+      img.src = '../assets/images/wood.png';
+      context.drawImage(img, 0, 0);
+  }
+
 }
