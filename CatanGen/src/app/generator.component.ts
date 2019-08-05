6e6406502e1e95b5c73d158757049f709c9795d6
Arianna Tuwiner <atuwiner97@gmail.com>
2019-08-05 16:05:24 -0400

    Got super basic generation working

--- CatanGen/src/app/generator.component.ts
+++ CatanGen/src/app/generator.component.ts
@@ -10,21 +10,12 @@ import * as _ from 'lodash';
 
 })
 export class GeneratorComponent {
-
-  normalNumbers: Array<number>;
-  normalResources: Array<string>;
-  numNumbers: number;
-  numResources: number;
-
-  constructor() {
-    // Desert tile will not be included in the resource array
-    // It will be pushed into the final array randomly.
-    this.normalNumbers = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
-    this.normalResources = ['ore', 'ore', 'ore', 'brick', 'brick', 'brick', 'sheep', 'sheep', 'sheep', 'sheep',
-    'wood', 'wood', 'wood', 'wood', 'hay', 'hay', 'hay', 'hay'];
-    this.numNumbers = 18;
-    this.numResources = 18;
-  }
+  normalNumbers = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
+  normalResources = ['ore', 'ore', 'ore', 'brick', 'brick', 'brick', 'sheep', 'sheep', 'sheep', 'sheep',
+  'wood', 'wood', 'wood', 'wood', 'hay', 'hay', 'hay', 'hay'];
+  numNumbers = 18;
+  numResources = 18;
+  tiles = [];
 
   // When Generate Board button is clicked, this function is called.
   generateBoard() {
@@ -34,15 +25,14 @@ export class GeneratorComponent {
     const shuffledNumbers = _.shuffle(this.normalNumbers);
     const shuffledResources = _.shuffle(this.normalResources);
 
-    // Create an empty array to hold the resulting tiles (resource + number combo).
-    const tiles = [];
     // Iterate through the shuffledNumbers and create tiles with the corresponding
     // resource from shuffledNumbers.
+    this.tiles = [];
     for (let i = 0; i < this.numNumbers; i++) {
       const numb = shuffledNumbers[i];
       const resource = shuffledResources[i];
       const tile = {num: numb, res: resource};
-      tiles.push(tile);
+      this.tiles.push(tile);
     }
 
     // Need to push a desert tile.
@@ -50,16 +40,21 @@ export class GeneratorComponent {
     // Get a random number to represent the random index we will insert the desert at.
     const randomNum = Math.floor(Math.random() * 19);
     // Insert the desert tile.
-    tiles.splice(randomNum, 0, desert);
-    console.log(tiles);
-    return tiles;
+    this.tiles.splice(randomNum, 0, desert);
+    console.log(this.tiles);
+    this.printTiles();
   }
 
   // This method will actually 'print' the randomized tiles to the DOM.
   // It will take in the tiles array as an argument.
-  printTiles(tiles: Array<object>) {
-
-
+  printTiles() {
+    const numTiles = 19;
+    for (let i = 0; i < numTiles; i ++) {
+      const element = document.getElementById('tile' + i);
+      if (element.classList.length === 2) {
+        element.classList.remove(element.classList.item(1));
+      }
+      element.classList.add(this.tiles[i].res);
+    }
   }
-
 }
