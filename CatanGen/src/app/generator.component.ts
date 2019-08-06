86f4c5b378275c3f6e146e5009b9f49bec67cfe5
Arianna Tuwiner <atuwiner97@gmail.com>
2019-08-06 12:08:56 -0400

    Got numbers to SPAWN

--- CatanGen/src/app/generator.component.ts
+++ CatanGen/src/app/generator.component.ts
@@ -1,5 +1,4 @@
-import { Component } from '@angular/core';
-import { BrowserDynamicTestingModule } from '@angular/platform-browser-dynamic/testing';
+import { Component, OnInit } from '@angular/core';
 import * as _ from 'lodash';
 
 
@@ -13,10 +12,12 @@ export class Tile {
     this.res = res;
   }
 
+  // Returns the number of the Tile object.
   getNumber() {
     return this.num;
   }
 
+  // Returns the resource of the Tile object.
   getResource() {
     return this.res;
   }
@@ -29,14 +30,20 @@ export class Tile {
   styleUrls: ['./generator.component.css']
 
 })
-export class GeneratorComponent {
+export class GeneratorComponent implements OnInit {
   normalNumbers = [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12];
   normalResources = ['ore', 'ore', 'ore', 'brick', 'brick', 'brick', 'sheep', 'sheep', 'sheep', 'sheep',
   'wood', 'wood', 'wood', 'wood', 'hay', 'hay', 'hay', 'hay'];
   numNumbers = 18;
   numResources = 18;
-  tiles = [];
+  tiles: Array<Tile>;
 
+  // This is a lifecycle hook function.
+  // To avoid errors with the ngIf directives in the HTML file,
+  // call generateBoard() as soon as the page is initialized.
+  ngOnInit() {
+    this.generateBoard();
+  }
 
   // When Generate Board button is clicked, this function is called.
   generateBoard() {
@@ -61,17 +68,18 @@ export class GeneratorComponent {
     const randomNum = Math.floor(Math.random() * 19);
     // Insert the desert tile.
     this.tiles.splice(randomNum, 0, desert);
-    console.log(this.tiles);
+    // console.log(this.tiles);
 
     // Call the printTiles function to then display the resources.
-    this.printTiles();
+    this.printResources();
+    // this.printNumbers();
   }
 
 
-  // This method will actually 'print' the randomized tiles to the DOM.
+  // This method will actually 'print' the randomized resources to the DOM.
   // It will use the global tiles variable, filled in the generateBoard() function.
   // I chose to keep this as a separate method b/c it will be easier for future.
-  printTiles() {
+  printResources() {
     const numTiles = 19;
     // Iterate through the tiles...
     for (let i = 0; i < numTiles; i ++) {
@@ -87,12 +95,37 @@ export class GeneratorComponent {
     }
   }
 
+  // This function was made to assign numbers to the chits.
+  // It's not working right now, so now I use text binding in the HTML file.
+  printNumbers() {
+    const numChits = 19;
+    // Iterate through the chits...
+    for (let i = 0; i < numChits; i++) {
+      // Grab a single chit by its id.
+      const element = document.getElementById('chit' + i);
+      // Change the inner text of the chit.
+      element.innerHTML = this.getNumberByIndex(i).toString();
+    }
+  }
+
+  // Returns the array of randomized Tile objects.
   getTiles() {
     return this.tiles;
   }
 
+  // Returns specified Tile object from the tiles array.
   getTileByIndex(idx) {
     return this.tiles[idx];
+  }
+
+  // Returns the resource of specified Tile object.
+  getResourceByIndex(idx) {
+    return this.tiles[idx].getResource();
+  }
+
+  // Returns the number of specified Tile object.
+  getNumberByIndex(idx) {
+    return this.tiles[idx].getNumber();
   }
 
 }
