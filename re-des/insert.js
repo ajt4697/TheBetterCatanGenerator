086982b39550421d0270915dd2e694a11688c036
Jamison Bunge <JamisonBunge@outlook.com>
2019-10-11 12:32:49 -0400

    testing something

--- re-des/insert.js
+++ re-des/insert.js
@@ -109,6 +109,17 @@ generateTiles();
 
 
 
-        // theTile.style.backgroundImage = `url(./assets/${tile.resource}.png)`;
+// theTile.style.backgroundImage = `url(./assets/${tile.resource}.png)`;
 
-  //  document.getElementById(`tile-${id}`).classList.add(tile.resource);
+//  document.getElementById(`tile-${id}`).classList.add(tile.resource);
+
+// First we get the viewport height and we multiple it by 1% to get a value for a vh unit
+let vh = window.innerHeight * 0.01;
+// Then we set the value in the --vh custom property to the root of the document
+document.documentElement.style.setProperty('--vh', `${vh}px`);
+
+window.addEventListener('resize', () => {
+    // We execute the same script as before
+    let vh = window.innerHeight * 0.01;
+    document.documentElement.style.setProperty('--vh', `${vh}px`);
+});
