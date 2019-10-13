db3d4950178587e799436ea0e9f8368ff52b7ac6
Arianna Tuwiner <atuwiner97@gmail.com>
2019-10-12 22:56:25 -0400

    SHUFFLE WORKS BAYBYYYYYYYYY

--- re-des/insert.js
+++ re-des/insert.js
@@ -1,5 +1,13 @@
+
+let tileOffsetCSS = ["top:20.4%;left:33%;", "top:20.4%;", "top:20.4%;left:67%;", //first row
+    "top:35.2%;left:24.5%;", "top:35.2%;left:41.5%;", "top:35.2%;left:58.5%;", "top:35.2%;left:75.5%;", //second row
+    "left:16%;", "left:33%;", " ", "left:67%;", "left:84%;", //third row
+    "top:64.8%;left:24.5%;", "top:64.8%;left:41.5%;", "top:64.8%;left:58.5%;", "top:64.8%;left:75.5%;", //forth row
+    "top:79.6%;left:33%;", "top:79.6%;", "top:79.6%;left:67%;"]; //fifth row
+
+
 //keeping this here for now bc im having trouble importing it
-let resourceTypes = ["ore", "sheep", "brick", "wood", "wheat"];
+let resourceTypes = ["ore", "sheep", "brick", "wood", "wheat","desert"];
 let prob = ["", "", ".", "..", "...", "....", ".....", "", "....", "...", "..", "..", ".",]
 state = {
     numArray: [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12],
@@ -10,9 +18,25 @@ state = {
 
 }
 
+// This shuffle algorithm is used to shuffle both the resources and the numbers.
+// Following that, it is used to shuffled the Tiles array created in gen().
+// This algorithm shuffles the array in place.
+// Algorithm from here: https://stackoverflow.com/questions/6274339/how-can-i-shuffle-an-array
+let shuffle = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
+
+
+
+// This method just creates and returns the array of tiles (tile information)
+// that generateTiles() uses to present the tiles to the board in HTML form.
 let gen = () => {
-    let randomNumbers = this.state.numArray
-    let randomResources = this.state.resourceArray
+    let randomNumbers = shuffle(this.state.numArray)
+    let randomResources = shuffle(this.state.resourceArray)
     let probArr = this.state.prob;
     let tiles = []
 
@@ -24,24 +48,17 @@ let gen = () => {
         tiles.push(tile)
     }
 
-    let dessert = Object()
-    dessert.resource = "desert"
-    dessert.chit = ""
-    dessert.probability = ""
-    tiles.push(dessert)
-
+    let desert = Object()
+    desert.resource = "desert"
+    desert.chit = ""
+    desert.probability = ""
+    tiles.push(desert)
     console.log(tiles)
-    return tiles
+    return shuffle(tiles)
 }
 
 
 ////end what should not be in this file
-
-let tileOffsetCSS = ["top:20.4%;left:33%;", "top:20.4%;", "top:20.4%;left:67%;", //first row
-    "top:35.2%;left:24.5%;", "top:35.2%;left:41.5%;", "top:35.2%;left:58.5%;", "top:35.2%;left:75.5%;", //second row
-    "left:16%;", "left:33%;", " ", "left:67%;", "left:84%;", //third row
-    "top:64.8%;left:24.5%;", "top:64.8%;left:41.5%;", "top:64.8%;left:58.5%;", "top:64.8%;left:75.5%;", //forth row
-    "top:79.6%;left:33%;", "top:79.6%;", "top:79.6%;left:67%;"]; //fifth row
 
 let buildBoard = () => {
 
@@ -58,15 +75,19 @@ let buildBoard = () => {
     }
 }
 
-let test = () => {
+// This method is called when the button is pressed.
+// This is how the DOM interacts with the JS part.
+let generateBoard = () => {
     event.preventDefault();
     console.log(`test`)
     //state.numArray = state.numArray.sort((a, b) => { 0.5 - Math.random() })
     // state.resourceArray.sort((a, b) => { 0.5 - Math.random() })
-    state.numArray.reverse();
+    // state.numArray.reverse();
     generateTiles();
 }
 
+// This handles the HTML component/displaying of the tiles using the array
+// returned by gen().
 let generateTiles = () => {
 
     let tiles = gen();
@@ -104,6 +125,9 @@ let generateTiles = () => {
     }
 }
 
+// These functions are called initially upon loading the page for the first time.
+// buildBoard() creates the HTML hexagon & chit elements for the board.
+// generateTiles() handles the displaying of the resources and numbers.
 buildBoard();
 generateTiles();
 
