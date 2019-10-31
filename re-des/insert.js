80d6c971866df9f8fbfdb2f79aa67edcbf9b3c9c
Jamison Bunge <JamisonBunge@outlook.com>
2019-10-31 16:43:35 -0400

    seeing if new board wrapper issue is fixed

--- re-des/insert.js
+++ re-des/insert.js
@@ -1,20 +1,20 @@
 
 let tileOffsetCSS = ["top:20.4%;left:33%;", "top:20.4%;", "top:20.4%;left:67%;", //first row
-    "top:35.2%;left:24.5%;", "top:35.2%;left:41.5%;", "top:35.2%;left:58.5%;", "top:35.2%;left:75.5%;", //second row
-    "left:16%;", "left:33%;", " ", "left:67%;", "left:84%;", //third row
-    "top:64.8%;left:24.5%;", "top:64.8%;left:41.5%;", "top:64.8%;left:58.5%;", "top:64.8%;left:75.5%;", //forth row
-    "top:79.6%;left:33%;", "top:79.6%;", "top:79.6%;left:67%;"]; //fifth row
+  "top:35.2%;left:24.5%;", "top:35.2%;left:41.5%;", "top:35.2%;left:58.5%;", "top:35.2%;left:75.5%;", //second row
+  "left:16%;", "left:33%;", " ", "left:67%;", "left:84%;", //third row
+  "top:64.8%;left:24.5%;", "top:64.8%;left:41.5%;", "top:64.8%;left:58.5%;", "top:64.8%;left:75.5%;", //forth row
+  "top:79.6%;left:33%;", "top:79.6%;", "top:79.6%;left:67%;"]; //fifth row
 
 
 //keeping this here for now bc im having trouble importing it
-let resourceTypes = ["ore", "sheep", "brick", "wood", "wheat","desert"];
+let resourceTypes = ["ore", "sheep", "brick", "wood", "wheat", "desert"];
 let prob = ["", "", ".", "..", "...", "....", ".....", "", "....", "...", "..", "..", ".",]
 state = {
-    numArray: [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12],
-    resourceArray: ["ore", "ore", "ore", "brick", "brick", "brick", "sheep"
-        , "sheep", "sheep", "sheep", "wood", "wood", "wood", "wood",
-        "wheat", "wheat", "wheat", "wheat",],
-    prob: ["", "", ".", "..", "...", "....", ".....", "", ".....", "....", "...", "..", ".",]
+  numArray: [2, 3, 3, 4, 4, 5, 5, 6, 6, 8, 8, 9, 9, 10, 10, 11, 11, 12],
+  resourceArray: ["ore", "ore", "ore", "brick", "brick", "brick", "sheep"
+    , "sheep", "sheep", "sheep", "wood", "wood", "wood", "wood",
+    "wheat", "wheat", "wheat", "wheat",],
+  prob: ["", "", ".", "..", "...", "....", ".....", "", ".....", "....", "...", "..", ".",]
 
 }
 
@@ -22,25 +22,25 @@ state = {
 // Format --> tile # : [adjacent tiles]
 let adjacencyList = {
 
-  0 : [1,3,4],
-  1 : [0,2,4,5],
-  2 : [1,5,6],
-  3 : [0,4,7,8],
-  4 : [0,1,3,5,8,9],
-  5 : [1,2,4,6,9,10],
-  6 : [2,5,10,11],
-  7 : [3,8,12],
-  8 : [3,4,7,9,12,13],
-  9 : [4,5,8,10,13,14],
-  10 : [5,6,9,11,14,15],
-  11 : [6,10,15],
-  12 : [7,8,13,16],
-  13 : [8,9,12,14,16,17],
-  14 : [9,10,13,15,17,18],
-  15 : [10,11,14,18],
-  16 : [12,13,17],
-  17 : [13,14,16,18],
-  18 : [14,15,17]
+  0: [1, 3, 4],
+  1: [0, 2, 4, 5],
+  2: [1, 5, 6],
+  3: [0, 4, 7, 8],
+  4: [0, 1, 3, 5, 8, 9],
+  5: [1, 2, 4, 6, 9, 10],
+  6: [2, 5, 10, 11],
+  7: [3, 8, 12],
+  8: [3, 4, 7, 9, 12, 13],
+  9: [4, 5, 8, 10, 13, 14],
+  10: [5, 6, 9, 11, 14, 15],
+  11: [6, 10, 15],
+  12: [7, 8, 13, 16],
+  13: [8, 9, 12, 14, 16, 17],
+  14: [9, 10, 13, 15, 17, 18],
+  15: [10, 11, 14, 18],
+  16: [12, 13, 17],
+  17: [13, 14, 16, 18],
+  18: [14, 15, 17]
 
 }
 
@@ -74,10 +74,10 @@ let shuffle = (arr) => {
 // If it goes through whole array and does not encounter adjacent 6 and 8s, return false,
 // AKA there are no adjacencies present.
 let passedAdjacencyTest = (tilesArr) => {
-  for (let [boardLocation,  tile] of tilesArr.entries()) {
-    if (tile.chit== 6 || tile.chit == 8) {
+  for (let [boardLocation, tile] of tilesArr.entries()) {
+    if (tile.chit == 6 || tile.chit == 8) {
       for (adj of adjacencyList[boardLocation]) {
-        if ( tilesArr[adj].chit == 6 || tilesArr[adj].chit == 8) return false
+        if (tilesArr[adj].chit == 6 || tilesArr[adj].chit == 8) return false
       }
     }
   }
@@ -86,19 +86,14 @@ let passedAdjacencyTest = (tilesArr) => {
 
 let passedResourceCheck = (tilesArr) => {
   for (let [boardLocation, tile] of tilesArr.entries()) {
-      let resource = tile.resource
-      let count = 1;
-      for (adj of adjacencyList[boardLocation]) {
-         // console.log (` ${resource} == ${tilesArr[adj].resource}`)
-          if(resource == tilesArr[adj].resource){
-            // console.log (`HIT`)
-            count++
-          }
+    let resource = tile.resource
+    let count = 1;
+    for (adj of adjacencyList[boardLocation]) {
+      if (resource == tilesArr[adj].resource) {
+        count++
       }
-      // console.log(count)
-      if (count > 2) {
-        return false
-      }
+    }
+    if (count > 2) return false
   }
   return true
 }
@@ -108,27 +103,27 @@ let passedResourceCheck = (tilesArr) => {
 // This method just creates and returns the array of tiles (tile information)
 // that generateTiles() uses to present the tiles to the board in HTML form.
 let gen = () => {
-    let randomNumbers = shuffle(this.state.numArray)
-    let randomResources = shuffle(this.state.resourceArray)
-    let probArr = this.state.prob;
-    let tiles = []
+  let randomNumbers = shuffle(this.state.numArray)
+  let randomResources = shuffle(this.state.resourceArray)
+  let probArr = this.state.prob;
+  let tiles = []
 
-    for (let x in randomNumbers) {
-        let tile = Object()
-        tile.chit = randomNumbers[x]
-        tile.resource = randomResources[x]
-        tile.probability = probArr[tile.chit]
-        tiles.push(tile)
-    }
+  for (let x in randomNumbers) {
+    let tile = Object()
+    tile.chit = randomNumbers[x]
+    tile.resource = randomResources[x]
+    tile.probability = probArr[tile.chit]
+    tiles.push(tile)
+  }
 
-    let desert = Object()
-    desert.resource = "desert"
-    desert.chit = ""
-    desert.probability = ""
-    tiles.push(desert)
+  let desert = Object()
+  desert.resource = "desert"
+  desert.chit = ""
+  desert.probability = ""
+  tiles.push(desert)
 
-    //console.log(tiles)
-    return shuffle(tiles)
+  //console.log(tiles)
+  return shuffle(tiles)
 }
 
 
@@ -136,28 +131,28 @@ let gen = () => {
 
 let buildBoard = () => {
 
-    ///////messeses with overflow so its commented out
-    // document.getElementById('board').innerHTML += `<div id="test-outer"></div>`
+  ///////messeses with overflow so its commented out
+  document.getElementById('board').innerHTML += `<div id="test-outer"></div>`
 
-    for (let [id, css] of tileOffsetCSS.entries()) {
+  for (let [id, css] of tileOffsetCSS.entries()) {
 
-        document.getElementById('board').innerHTML +=
-            `<div class="hex" style="${css}" id="tile-${id}")>
+    document.getElementById('board').innerHTML +=
+      `<div class="hex" style="${css}" id="tile-${id}")>
                 <div class="circle" id="circle-${id}">
                 </div>
             </div>`
-    }
+  }
 }
 
 // This method is called when the button is pressed.
 // This is how the DOM interacts with the JS part.
 let generateBoard = () => {
-    event.preventDefault();
-    // console.log(`test`)
-    //state.numArray = state.numArray.sort((a, b) => { 0.5 - Math.random() })
-    // state.resourceArray.sort((a, b) => { 0.5 - Math.random() })
-    // state.numArray.reverse();
-    generateTiles();
+  event.preventDefault();
+  // console.log(`test`)
+  //state.numArray = state.numArray.sort((a, b) => { 0.5 - Math.random() })
+  // state.resourceArray.sort((a, b) => { 0.5 - Math.random() })
+  // state.numArray.reverse();
+  generateTiles();
 
 
 
@@ -175,43 +170,43 @@ let generateTiles = () => {
   //     console.log("new board (before checking)")
   //     console.log(tiles)
   //   }
-    let tiles = gen();
-      do {
-        tiles = gen();
-      } while(!passedAdjacencyTest(tiles) || !passedResourceCheck(tiles))
+  let tiles;;
+  do {
+    tiles = gen();
+  } while (!passedAdjacencyTest(tiles) || !passedResourceCheck(tiles))
 
 
-    for (let [id, tile] of tiles.entries()) {
+  for (let [id, tile] of tiles.entries()) {
 
-        let theTile = document.getElementById(`tile-${id}`);
-        let theCircle = document.getElementById(`circle-${id}`);
+    let theTile = document.getElementById(`tile-${id}`);
+    let theCircle = document.getElementById(`circle-${id}`);
 
-        //console.log(theTile.classList)
+    //console.log(theTile.classList)
 
 
-        for (let currentResource of theTile.classList) {
-            if (resourceTypes.includes(currentResource)) {
-                theTile.classList.remove(currentResource);
-                break
-            }
-        }
-        theTile.classList.add(tile.resource);
-        theCircle.innerHTML = `<h2>${tile.chit}</h2>`
-
-        if (tile.chit == 8 || tile.chit == 6) {
-            theTile.classList.add("high-prob")
-        } else {
-            theTile.classList.remove("high-prob")
-            //theTile.style.color = "black";
-        }
-
-        if (tile.resource == "desert") {
-            theCircle.classList.add("desert-chit")
-        } else {
-            theCircle.classList.remove("desert-chit")
-            theCircle.innerHTML += `<h3>${tile.probability}</h3>`
-        }
+    for (let currentResource of theTile.classList) {
+      if (resourceTypes.includes(currentResource)) {
+        theTile.classList.remove(currentResource);
+        break
+      }
     }
+    theTile.classList.add(tile.resource);
+    theCircle.innerHTML = `<h2>${tile.chit}</h2>`
+
+    if (tile.chit == 8 || tile.chit == 6) {
+      theTile.classList.add("high-prob")
+    } else {
+      theTile.classList.remove("high-prob")
+      //theTile.style.color = "black";
+    }
+
+    if (tile.resource == "desert") {
+      theCircle.classList.add("desert-chit")
+    } else {
+      theCircle.classList.remove("desert-chit")
+      theCircle.innerHTML += `<h3>${tile.probability}</h3>`
+    }
+  }
 }
 
 // These functions are called initially upon loading the page for the first time.
@@ -232,7 +227,7 @@ let vh = window.innerHeight * 0.01;
 document.documentElement.style.setProperty('--vh', `${vh}px`);
 
 window.addEventListener('resize', () => {
-    // We execute the same script as before
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+  // We execute the same script as before
+  let vh = window.innerHeight * 0.01;
+  document.documentElement.style.setProperty('--vh', `${vh}px`);
 });
